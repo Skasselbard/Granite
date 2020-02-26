35fd66966dfb8a487bedb1324c8bc2c0259f1040
tom <tom.meyer89@gmail.com>
2020-02-26 14:45:29 +0100

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Granite
 Find Deadlocks in Rust with Petri-Net Model checking (Master Thesis)
+This project was startet as part of my masters thesis "[A Petri-Net Semantics for Rust](https://github.com/Skasselbard/Granite/blob/master/doc/MasterThesis/main.pdf)".
 
 - used rust nightly can be found in the [rust-toolchain file](https://doc.rust-lang.org/nightly/edition-guide/rust-2018/rustup-for-managing-rust-versions.html#managing-versions)
 - rustc-dev component is needed ``rustup toolchain install [nightly-x-y-z] --component rustc-dev``
+- also the linker has to know about the lib folder from the sysroot fiting the toolchain.
+- some useful can be found in the script folder. This includes:
+    - an install script for the model checker LoLa
+    - a run script that can translate programs from ``./tests/sample_programs``
+    - and a script that can analyse the output
 
-# Master Thesis: A Petri-Net-Semantics for Rust (Eine Petri-Netz-Semantik für Rust)
-- part of the Documentation
--- scripts/run.py
+++ scripts/run.py
@@ -20,20 +20,8 @@ def run(filename):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to run granite with the correct configuration")
     parser.add_argument('-r', '--run', help="run granite on a file from tests/sample_programs")
-    # parser.add_argument('-u', '--unconditional-deadlock',action="store_true", help="Search for every deadlock. Even program termination is concidered a deadlock")
-    # parser.add_argument('-p', '--panic',action="store_true", help="Check if it is possible to reach a panic state")
-    # parser.add_argument('-n', '--neighbors', nargs="*", help="Generates a subnet with the given nodes and all its neighbors and visualizes it.")
-    # parser.add_argument('-v', '--visualize', action="store_true", help="visualize the graph in graphviz. Doesn't terminate (in time) for larger graphs")
 
     args = parser.parse_args()
 
     if args.run:
         run(args.run)
-    # if args.unconditional_deadlock:
-    #     unconditional_deadlock()
-    # if args.panic:
-    #     can_panic()
-    # if args.neighbors:
-    #     neighbors(args.neighbors)
-    # if args.visualize:
-    #     visualize("net.dot")
